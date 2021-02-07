3a77d8aa3d420bf7f0d3af91b114e5c2a6976cd2
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2021-02-07 14:40:54 -0500

    Updated Product repository to add a function to update product and functions to delete specific colors and sizes

--- src/main/kotlin/com/tolkiana/liquibasedemo/data/ProductRepository.kt
+++ src/main/kotlin/com/tolkiana/liquibasedemo/data/ProductRepository.kt
@@ -33,12 +33,8 @@ const val deleteAllProductSizes = """
     DELETE FROM product_sizes WHERE product_id = :productId
 """
 
-const val deleteProductColor = """
-    DELETE FROM product_colors WHERE product_id = $1 AND color_id = $2
-"""
-
-const val deleteProductSizes = """
-    DELETE FROM product_sizes WHERE product_id = $1 AND size_id = $2
+const val updateProduct = """
+    UPDATE products SET code = :code, description = :description WHERE id = :productId
 """
 
 interface ProductRepository: ReactiveCrudRepository<Product, Int>, CustomProductRepository {}
@@ -46,12 +42,13 @@ interface ProductRepository: ReactiveCrudRepository<Product, Int>, CustomProduct
 interface CustomProductRepository {
     fun findAll(): Flux<Product>
     fun save(product: Product): Mono<Product>
+    fun update(product: Product): Mono<Int>
     fun insertProductColors(productId: Number, colorIds: List<Number>): Flux<Number>
-    fun insertProductSize(productId: Number, sizeIds: List<Number>): Flux<Number>
-    fun deleteProductColors(productId: Number): Mono<Int>
-    fun deleteProductSizes(productId: Number): Mono<Int>
-    fun deleteProductColor(productId: Number, colorId: Number): Mono<Int>
-    fun deleteProductSize(productId: Number, colorId: Number): Mono<Int>
+    fun insertProductSizes(productId: Number, sizeIds: List<Number>): Flux<Number>
+    fun deleteAllProductColors(productId: Number): Mono<Int>
+    fun deleteAllProductSizes(productId: Number): Mono<Int>
+    fun deleteProductColors(productId: Number, colorIds: List<Number>): Mono<Void>
+    fun deleteProductSizes(productId: Number, sizesIds: List<Number>): Mono<Void>
 }
 
 // Custom Repositories need to en with "Impl" or everything explodes!
@@ -75,6 +72,14 @@ class CustomProductRepositoryImpl(
             .map { product.copy(id = it["id"] as Number?) }
     }
 
+    override fun update(product: Product): Mono<Int> {
+        return databaseClient.sql(updateProduct)
+            .bind("code", product.code)
+            .bind("description", product.description)
+            .bind("productId", product.id!!)
+            .fetch().rowsUpdated()
+    }
+
     override fun insertProductColors(productId: Number, colorIds: List<Number>): Flux<Number> {
         return databaseClient.inConnectionMany { connection ->
             val statement = connection.createStatement(insertProductColor)
@@ -87,7 +92,7 @@ class CustomProductRepositoryImpl(
         }
     }
 
-    override fun insertProductSize(productId: Number, sizeIds: List<Number>): Flux<Number> {
+    override fun insertProductSizes(productId: Number, sizeIds: List<Number>): Flux<Number> {
         return databaseClient.inConnectionMany { connection ->
             val statement = connection.createStatement(insertProductSize)
             sizeIds.forEach {
@@ -99,25 +104,37 @@ class CustomProductRepositoryImpl(
         }
     }
 
-    override fun deleteProductColors(productId: Number): Mono<Int> {
+    override fun deleteAllProductColors(productId: Number): Mono<Int> {
         return databaseClient
             .sql(deleteAllProductColors)
             .bind("productId", productId)
             .fetch().rowsUpdated()
     }
 
-    override fun deleteProductSizes(productId: Number): Mono<Int> {
+    override fun deleteAllProductSizes(productId: Number): Mono<Int> {
         return databaseClient
                 .sql(deleteAllProductSizes)
                 .bind("productId", productId)
                 .fetch().rowsUpdated()
     }
 
-    override fun deleteProductColor(productId: Number, colorId: Number): Mono<Int> {
-        TODO("Not yet implemented")
+    override fun deleteProductColors(productId: Number, colorIds: List<Number>): Mono<Void> {
+        return databaseClient.inConnection { connection ->
+            val batch = connection.createBatch()
+            colorIds.forEach {
+                batch.add("DELETE FROM product_colors WHERE product_id = $productId AND color_id = $it")
+            }
+            batch.execute().toFlux().then()
+        }
     }
 
-    override fun deleteProductSize(productId: Number, colorId: Number): Mono<Int> {
-        TODO("Not yet implemented")
+    override fun deleteProductSizes(productId: Number, sizesIds: List<Number>): Mono<Void> {
+        return databaseClient.inConnection { connection ->
+            val batch = connection.createBatch()
+            sizesIds.forEach {
+                batch.add("DELETE FROM product_sizes WHERE product_id = $productId AND size_id = $it")
+            }
+            batch.execute().toFlux().then()
+        }
     }
 }
