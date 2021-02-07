319bf8a4a26e5d1aaa9c7947bc0110ad111e70b9
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2021-02-06 20:37:27 -0500

    Don't show empty strings

--- src/main/kotlin/com/tolkiana/liquibasedemo/presentation/dtos/ColorDto.kt
+++ src/main/kotlin/com/tolkiana/liquibasedemo/presentation/dtos/ColorDto.kt
@@ -2,7 +2,7 @@ package com.tolkiana.liquibasedemo.presentation.dtos
 
 import com.fasterxml.jackson.annotation.JsonInclude
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class ColorDto(
     val id: Number,
     val code: String? = null,
