e113619b6f55f376fdd668fd7d9c1b8e3f5619e3
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2021-02-22 20:52:21 -0500

    Fixed mappers null pointer exception

--- src/main/kotlin/com/tolkiana/liquibasedemo/data/mappers/SizeMapper.kt
+++ src/main/kotlin/com/tolkiana/liquibasedemo/data/mappers/SizeMapper.kt
@@ -10,7 +10,7 @@ import java.util.function.BiFunction
 class SizeMapper: BiFunction<Row, Any, Size> {
     override fun apply(row: Row, o: Any): Size {
         return Size(
-            row.get("size_id", Int::class.java) ?: throw UnexpectedException(),
+            row.get("size_id", Number::class.java)?.toInt() ?: throw UnexpectedException(),
             row.get("size_code", String::class.java) ?: throw UnexpectedException(),
             row.get("sort_order", Number::class.java) ?: throw UnexpectedException()
         )
